372cf3ccc620e2cc6a48a81f6447932153e617e6
Souhail SOUID <mr.souid@live.fr>
2020-09-15 12:21:20 +0200

    config babel, add context and modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 
 import { Link, Router } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./searchParams";
+import ThemeContext from "./ThemeContext";
 const App = () => {
+  const themeHook = useState("peru");
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
